491a2bedb9210cdebb2a5c30f4ef2dbf1426eaca
Matilde Morrone <contact@morrone.dev>
2024-08-18 12:24:12 +0200

    Create basic hello world server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+
+    info!("Starting server at port 3000");
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
